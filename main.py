981ca8aeb95d73e7b67ed536585c16e928362e76
vyshnavi2404 <78897377+vyshnavi2404@users.noreply.github.com>
2026-06-14 19:18:28 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+def main():
+
+    print("ISFHC-GHI Framework")
+
+    print("Stage 1 : AdLOG-WF Preprocessing")
+
+    print("Stage 2 : Modified U-Net Segmentation")
+
+    print("Stage 3 : HOS Feature Extraction")
+
+    print("Stage 4 : Multitexton Feature Extraction")
+
+    print("Stage 5 : Color Feature Extraction")
+
+    print("Stage 6 : Hybrid Classification")
+
+    print("Stage 7 : Improved Score-Level Fusion")
+
+    print("Human Identification Completed")
+
+if __name__ == "__main__":
+    main()
